5216d179918f99b0934d1c28615dbc405527625b
venkeyboda07@gmail.com <venkeyboda07@gmail.com>
2025-01-19 12:52:24 +0530

    added init

--- Azure/Network_interface/variables.tf
+++ Azure/Network_interface/variables.tf
@@ -12,34 +12,34 @@ variable "location" {
 
 # Create a variable group for network interface
 variable "nic" {
-    type =object({
-      name = string
-      ip_configuration = object({
-        name = string
-        private_ip_address_allocation = string
+  type = object({
+    name = string
+    ip_configuration = object({
+      name                          = string
+      private_ip_address_allocation = string
 
-      })
-    }) 
+    })
+  })
 }
 
 # create a variable group for subnet id's
 variable "subnet_ids" {
-    type = string
-    description = "subnet id for network interface"  
+  type        = string
+  description = "subnet id for network interface"
 }
 
 # create a variable group for public ip address id's
 variable "pip-id" {
-    type = string
-    description = "public ip address id for network interface"
-  
+  type        = string
+  description = "public ip address id for network interface"
+
 }
 
 # create a variable group for network security group id's
 variable "nsg-id" {
-    type = string
-    description = "network security group id for newtork interface"
-  
+  type        = string
+  description = "network security group id for newtork interface"
+
 }
 # create a variable group for tags
 variable "tags" {
