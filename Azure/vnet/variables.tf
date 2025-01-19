2f74288ada83e39b32f69fcea2fc8f9788a184cd
venkeyboda07@gmail.com <venkeyboda07@gmail.com>
2025-01-19 11:07:46 +0530

    changes vnet

--- Azure/vnet/variables.tf
+++ Azure/vnet/variables.tf
@@ -1,13 +1,16 @@
+# create a variable group for resourse group
 variable "resource_group" {
   type        = string
   description = "name of the resource group"
 }
 
+# create a variable group for location
 variable "location" {
   type        = string
   description = "location of virtual network"
 }
 
+# create a variable group for virtual network  
 variable "network_info" {
   type = object({
     address_space = optional(list(string), ["10.0.0.0/16"])
@@ -17,6 +20,7 @@ variable "network_info" {
   description = "network information"
 }
 
+# create a variable group for tags
 variable "tags" {
   type = map(string)
   default = {
