dc687ff0f28d4689abb603a0f45e583401b41c7a
venkeyboda07@gmail.com <venkeyboda07@gmail.com>
2025-01-19 13:51:30 +0530

    changes

--- Azure/Network_interface/variables.tf
+++ Azure/Network_interface/variables.tf
@@ -30,7 +30,7 @@ variable "webserver_subnet_value" {
 }
 
 # create a variable group for subnet id's
-variable "subnet_id" {
+variable "subnets_id" {
   type        = string
   description = "subnet id for network interface"
 }
